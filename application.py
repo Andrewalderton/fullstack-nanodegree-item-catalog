e8a2d316a5cb5783a54e87058ab65ec6e65ed264
Andy Alderton <andrewalderton@sky.com>
2017-08-22 17:46:29 +0100

    Add dummy data and static files

---
+++ application.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+from flask import Flask, g, render_template, flash, redirect, url_for, abort
--- database_config.py
+++ database_config.py
@@ -13,8 +13,34 @@ Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    email = Column(String)
+
+
+class Book(Base):
+    __tablename__ = 'books'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    author = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    img = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        # return book data in serializable format
+        return {
+            'id': self.id,
+            'title': self.title,
+            'author': self.author,
+            'description': self.description,
+            'img': self.img,
+        }
 
 
 engine = create_engine('sqlite:///itemcatalog.db')
